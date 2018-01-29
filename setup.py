510ea01933010bee2cc7223afed1cfb4abf4c47a
luckyyinghuang <ying.huang@ucdconnect.ie>
2018-01-29 14:01:38 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(name="systeminfo1",
+      version="0.1",
+      description="Basic system information for COMP30670",
+      url="",
+      author="Aonghus Lawlor",
+      author_email="aonghua.lawlor@ucd.ie",
+      licence="GPL3",
+      packages=['systeminfo1'],
+      entry_points={'console_scripts':
+                    ['comp30670_systeminfo1=systeminfo1.main:main'] 
+                    },
+      
+          )
+        
---
+++ systeminfo/main.py
@@ -0,0 +1,13 @@
+'''
+Created on 29 Jan 2018
+
+@author: 25142
+'''
+import platform
+def main():
+    print(platform.platform())
+    return
+    
+if __name__ == '__main__':
+    main()
+    
